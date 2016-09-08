20f856a8b56d1751aaa9b22011b90231dff0db70
Jon Skarpeteig <jskarpet@cisco.com>
2016-09-08 08:49:39 +0200

    Basic test added

--- tests/test_jira-comment.py
+++ tests/test_jira-comment.py
@@ -1,11 +1,12 @@
 import os
 from os.path import dirname
 import unittest
+import subprocess
+
+root = dirname(dirname(os.path.realpath(__file__)))
+jira_comment = "%s/bin/jira-comment" % root
 
 
 class TestCLIParser(unittest.TestCase):
     def test_does_compile(self):
-        root = dirname(dirname(os.path.realpath(__file__)))
-        exitcode = os.system("%s/bin/jira-comment -h" % root)
-        self.assertEqual(0, exitcode)
-
+        output = subprocess.check_output("coverage run %s -h" % jira_comment, shell=True)
